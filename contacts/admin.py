07f715bea4c5f334a594921a2d61aaf4228f30f4
bhaceera <bhaceera@gmail.com>
2020-03-10 16:58:37 +0100

    add customer model

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Contact
-from .models import Trash
+
 
 admin.site.register(Contact)
-admin.site.register(Trash)
--- contacts/forms.py
+++ contacts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
-from .models import Contact
+from .models import Contact, Customer
 
 
 class ContactForm(forms.Form):
@@ -28,6 +28,36 @@ class ContactForm(forms.Form):
                 Column('sex', css_class='form-group col-md-4 mb-0'),
                 Column('sbu', css_class='form-group col-md-4 mb-0'),
                 Column('status', css_class='form-group col-md-4 mb-0'),
+                css_class='form-row'
+            ),
+            Submit('submit', 'ADD CONTACT')
+        )
+
+
+class CustomerForm(forms.Form):
+    class Meta:
+        model = Customer
+        fields = ['name', 'email', 'phone', 'address', 'position', 'sex', 'sbu', 'company']
+
+    def __init__(self, *args, **kwargs):
+        super(Customer, self).__init__(*args, **kwags)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('name', css_class='form-group col-md-4 mb-0'),
+                Column('email', css_class='form-group col-md-4 mb-0'),
+                Column('phone', css_class='form-group col-md-4 mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('address', css_class='form-group col-md-6 mb-0'),
+                Column('position', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            Row(
+                Column('sex', css_class='form-group col-md-4 mb-0'),
+                Column('sbu', css_class='form-group col-md-4 mb-0'),
+                Column('company', css_class='form-group col-md-4 mb-0'),
                 css_class='form-row'
             ),
             Submit('submit', 'ADD CONTACT')
